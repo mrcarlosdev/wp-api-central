3a7a10e5dab6ef7cf132a2bac251f2fd64cfe318
mrcarlosdev <carlosperez@microsoft.com>
2023-01-01 20:27:19 +0100

    Plugin info and structure

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+- (1 January 2023) Created repository and structure to allocate Wordpress plugin
---
+++ wp-api-central/wp-api-central.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @link              https://github.com/mrcarlosdev/wp-api-central
+ * @since             0.0.1
+ * @package           WP_API_Central
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP API Central
+ * Plugin URI:        https://github.com/mrcarlosdev/wp-api-central
+ * Description:       WP API Central is a WordPress plugin that allows you to connect with cloud and gateway providers to access documentation for configured APIs. With WP API Central, you can view all relevant information about your APIs in one place, using WordPress as your documentation center.
+ * Version:           0.0.1
+ * Author:            Carlos Pérez
+ * Author URI:        https://es.linkedin.com/in/mrcarlosdev
+ * License:           MIT License
+ * License URI:       https://github.com/mrcarlosdev/wp-api-central/blob/main/LICENSE
+ * Text Domain:       wp-api-central
+ */
